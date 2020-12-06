3166fb4d891c804ef6212bb97e941f49ecc2274f
ileods <72619044+ileods@users.noreply.github.com>
2020-12-06 19:37:03 +0500

    Update mypage.js

--- static/main/js/mypage.js
+++ static/main/js/mypage.js
@@ -1,9 +1,6 @@
 const openForm = document.querySelector('.ram');
 const clickedForm = document.querySelector('.open-ram-click')
 const clickedCross = document.querySelector('.close');
-const toggleLike = document.querySelector('.like');
-//const redHeart = document.querySelector('.red');
-//const whiteHeart = document.querySelector ('.white');
 const appElem = document.querySelector('.app');
 const appFormElem =  document.querySelector('.app-form');
 const clickedCom = document.querySelector('#open-comment');
@@ -12,10 +9,6 @@ let comBool = false;
 
 
 const init = () => {
-  //redHeart = document.querySelector('.red');
-  //thisRedHeart = this.document.querySelector('.red');
-  //whiteHeart = document.querySelector ('.white');
-  //thisWhiteHeart = this.document.querySelector ('.white');
 
 
   clickedForm.addEventListener('click', event => {
@@ -47,20 +40,7 @@ const init = () => {
       openCom.classList.remove('visible');
     }
   });
-
-  //thisWhiteHeart.addEventListener('click', event =>{
-  //  console.log('fuck');
-  //  event.preventDefault();
-  //  event.target.classList.add('invisible');
-  //  thisRedHeart.classList.add('visible');
-  //});
-  //thisRedHeart.addEventListener('click', event =>{
-  //  console.log('shit');
-  //  event.preventDefault();
-  //  event.target.classList.remove('visible');
-  //  thisWhiteHeart.classList.remove('invisible');
-  //});
-
+  
 };
 var inputs = document.querySelectorAll('.inputfile');
 Array.prototype.forEach.call(inputs, function(input)
@@ -81,36 +61,3 @@ Array.prototype.forEach.call(inputs, function(input)
 });
 })
 init();
-
-
-//Vue.component('tweet', {
-// props: ['tweetText'],
-// data: function () {
-//   return {
-//     charactersRemaining: 280,
-//     commentText: '',
-//     liked: false
-//   }
-// },
-// methods: {
-//   countCharacters: function() {
-//     this.charactersRemaining = 280 - this.commentText.length
-//   },
-//   toggleLike: function() {
-//     this.liked = !this.liked
-//   }
-// },
- // template:
-//})
-//new Vue({
-//
- // data: {
- // el: '#app',
- //   tweets: [
- //     { id: 1, tweetText: 'hello! it was a hard run, because it was raining '},
- //     { id: 2, tweetText: 'hi! today I was preparing for the marathon'},
- //     {id: 3, tweetText:'i run 10 km'},
- //     {id: 4, tweetText: 'my friend and I ran 5 km at speed, I won'}
- //   ]
- // }
-//})
